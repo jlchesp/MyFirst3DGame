b3ee7863a4a8411107100acf072546f9192033b2
jlchacon <jlchacon@isotrol.com>
2020-05-08 19:50:05 +0200

    Juego terminado

--- Assets/Coin/Coin.cs
+++ Assets/Coin/Coin.cs
@@ -4,24 +4,48 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    public static int coinsCount = 0;
+
     // Start is called before the first frame update
-    void Start() {
-        Debug.Log("El juego ha comenzado");
+    void Start()
+    {
+        Coin.coinsCount++;
+        Debug.Log("The game has started and now there are " + Coin.coinsCount + " coins");
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update() { }
 
-    }
-    
-    /*Método que se llama automáticamente cuando otro collider
-    entra en contacto con el que tiene asignado este script*/
-    private void OnTriggerEnter(Collider otherCollider) {
+    /*Method that is called automatically when another collider 
+    contact the one assigned this script*/
+    private void OnTriggerEnter(Collider otherCollider)
+    {
 
-        if(otherCollider.CompareTag("Player")) { //Condicional que recoge el tag asignado al jugador
-            Debug.Log("Algo ha colisionado con la moneda");
-            Destroy(gameObject); //Destruye el gameObject que tiene asignado este script
+        if (otherCollider.CompareTag("Player"))
+        { //Conditional that collects the tag assigned to the player
+
+            Coin.coinsCount--; //Reverse counter for when you collect coins
+
+            if (Coin.coinsCount == 0)
+            {
+
+                Debug.Log("You win!!");
+                GameObject gameManager = GameObject.Find("GameManager"); //We instantiate our gameManager to destroy it once all the coins have been collected
+                Destroy(gameManager);
+
+                GameObject[] fireworksSystem = GameObject.FindGameObjectsWithTag("Fireworks");
+
+                /*Loop that goes through our fireworksSystem array
+                that contains all the gameObjects with the tag "FireWorks"*/
+
+                foreach (GameObject fireworks in fireworksSystem)
+                {
+                    fireworks.GetComponent<ParticleSystem>().Play(); //Once finished, we start the fireworks
+                }
+            }
+
+            Destroy(gameObject); //Destroy the gameObject assigned to this script
         }
     }
-    
+
 }
---
+++ Assets/Manager/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float maxTime = 60f;
+    private float countDown = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countDown = maxTime; //The countdown starts at maxTime
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        countDown -= Time.deltaTime; //To our countdown we less the rendering time in seconds of frames (deltaTime)
+        Debug.Log(countDown);
+
+        if (countDown <= 0)
+        {
+
+            Debug.Log("YOU HAVE RUN OUT OF TIME... You lose!!");
+            Coin.coinsCount = 0;
+
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+}
